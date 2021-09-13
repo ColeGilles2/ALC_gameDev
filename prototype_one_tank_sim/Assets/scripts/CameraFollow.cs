8a7b387516e303f017f3b45ae62785e50f519a3b
Cole Gillespie <poolsurfer12@gmail.com>
2021-09-13 08:58:41 -0600

    bug ixes woth crates and stuff and a bllion tank colliders

--- prototype_one_tank_sim/Assets/scripts/CameraFollow.cs
+++ prototype_one_tank_sim/Assets/scripts/CameraFollow.cs
@@ -6,7 +6,7 @@ public class CameraFollow : MonoBehaviour {
 
     public GameObject player;
 
-    private Vector3 offset = new Vector3(0, 80, -120);
+    private Vector3 offset = new Vector3(0, 90, -145);
 
     // Update is called once per frame
     void Update() {
