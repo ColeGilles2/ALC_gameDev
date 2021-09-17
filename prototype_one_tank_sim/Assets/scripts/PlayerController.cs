0ea4a9161101931c30e010c026d606dedd86ad31
Cole Gillespie <poolsurfer12@gmail.com>
2021-09-17 08:34:14 -0600

    cleaned up and finished

--- prototype_one_tank_sim/Assets/scripts/PlayerController.cs
+++ prototype_one_tank_sim/Assets/scripts/PlayerController.cs
@@ -12,13 +12,6 @@ public class PlayerController : MonoBehaviour {
     // forward and back
     public float vInput;
 
-    // projectile
-
-    // Start is called before the first frame update
-    void Start() {
-        
-    }
-
     // Update is called once per frame
     void Update() {
         // button press values for horizontal and vertical inputs
