db46886921c874cf10696c6dff824fdbc195cfa1
Cole Gillespie <poolsurfer12@gmail.com>
2021-09-28 09:21:50 -0600

    enemy go BRRTRRRRRRRRR

--- prototype2_topDownGame/Assets/scripts/enemyAI.cs
+++ prototype2_topDownGame/Assets/scripts/enemyAI.cs
@@ -4,19 +4,36 @@ using UnityEngine;
 
 public class enemyAI : MonoBehaviour {
 
+    public Transform player;
+    public float moveSpeed = 2.0f;
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
     // Start is called before the first frame update
     void Start() {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update() {
-
+        // enemy pos - player pos
+        Vector2 direction = player.position - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        rb.rotation = angle;
+        direction.Normalize();
+        movement = direction;
     }
 
+    //occurs at a fixed rate per frame
+    void FixedUpdate() {
+        moveEnemy(movement);
+    }
 
-//not working
-    void onTrigger(Collider2D other) {
+    void moveEnemy(Vector2 direction) {
+        rb.MovePosition((Vector2)transform.position + (direction * moveSpeed * Time.deltaTime));
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.CompareTag("Bullet")) {
             Destroy(gameObject);
         }
