1dbbae9af9405d681aa9bdbc7548a5ca851f0108
Cole Gillespie <poolsurfer12@gmail.com>
2021-10-08 09:20:51 -0600

    jump and camera stuff

--- prototype3_firstPersonGame/Assets/scripts/PlayerController.cs
+++ prototype3_firstPersonGame/Assets/scripts/PlayerController.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour {
     // movement 
     public float moveSpeed; //how fast move
-    public float jumpForce; // how shigh can jumpp
+    public float jumpForce; // how high can jumpp
 
     //camera
     public float lookSensetivity; //sensetivity
-    public float maxLookX; //how high you can look
-    public float minLookX; // how low you can look
+    public float maxLookX; //how low you can look
+    public float minLookX; // how high you can look
     private float rotX; // current x position
 
     //componemts
     private Camera cam; // declares a camera
     private Rigidbody rb; // finds rigidbody
     
-    void awake() {
+    void Awake() {
         //get components
         cam = Camera.main;
         rb = GetComponent<Rigidbody>();
@@ -26,13 +26,38 @@ public class PlayerController : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         Move();
+        CamLook();
+        if (Input.GetButtonDown("Jump"))
+            Jump();
     }
 
     void Move() {
         float x = Input.GetAxis("Horizontal") * moveSpeed;
         float z = Input.GetAxis("Vertical") * moveSpeed;
 
-        rb.velocity = new Vector3(x, rb.velocity.y, z);
+        Vector3 dir = transform.right * x + transform.forward * z;
+        dir = rb.velocity.y;
+        rb.velocity = dir;
     } 
+
+    void Jump() {
+        //cast ray to ground
+        Ray ray = new Ray(transform.position, Vector3.down);
+        //check ray lenght to jump
+        if (Physics.Raycast(ray, 1.1f))
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+    }
+
+    void CamLook() {
+        float y = Input.GetAxis("Mouse X") * lookSensetivity;
+        rotX += Input.GetAxis("Mouse Y") * lookSensetivity; 
+
+        //clamp the verticle rotation of camera
+        rotX = Mathf.Clamp(rotX, minLookX, maxLookX);
+
+        //applying the rotation to camera
+        cam.transform.localRotation = Quaternion.Euler(-rotX, 0, 0);
+        transform.eulerAngles += Vector3.up * y;
+    }
 
 }
