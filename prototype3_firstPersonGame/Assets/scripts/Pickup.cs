c0ec30e31705b45784065b18bdc534d51ffd7cd5
Cole Gillespie <poolsurfer12@gmail.com>
2021-11-18 09:21:14 -0700

    cool things and a few particles

--- prototype3_firstPersonGame/Assets/scripts/Pickup.cs
+++ prototype3_firstPersonGame/Assets/scripts/Pickup.cs
@@ -12,9 +12,17 @@ public class Pickup : MonoBehaviour {
     public PickupType type;
     public int value;
 
+    [Header ("Bobbing Anim")]
+    public float rotationSpeed;
+    public float bobSpeed;
+    public float bobHeight;
+
+    private Vector3 startPos;
+    private bool bobbingUp;
+
     // Start is called before the first frame update
     void Start() {
-        
+        startPos = transform.position;
     }
 
     void OnTriggerEnter(Collider other) {
@@ -37,6 +45,14 @@ public class Pickup : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
-        
+        //Rotating
+        transform.Rotate(Vector3.up, rotationSpeed * Time.deltaTime);
+
+        //Bob up and down
+        Vector3 offset = (bobbingUp == true ? new Vector3(0,bobHeight / 2, 0) : new Vector3(0, -bobHeight / 2, 0));
+        transform.position = Vector3.MoveTowards(transform.position, startPos + offset, bobSpeed * Time.deltaTime);
+
+        if (transform.position == startPos + offset)
+            bobbingUp = !bobbingUp;
     }
 }
